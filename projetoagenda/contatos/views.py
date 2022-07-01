cb6f6cb1a03157a9d194663611f64f445ba60869
Italo SF <italosilvafernandesprog@gmail.com>
2022-06-30 23:56:53 -0300

    Commit Inicial, Estudei até paginação.

---
+++ projetoagenda/contatos/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Contato
+from django.core.paginator import Paginator
+
+
+def index(request):
+    contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos
+    })
+
+
+def ver_contato(request, contato_id):
+        contato = get_object_or_404(Contato, id=contato_id)
+        return render(request, 'contatos/ver_contato.html', {
+            'contato': contato
+        })
+
+# from django.http import Http404
+#
+# def ver_contato(request, contato_id):
+#     try:
+#         contato = Contato.objects.get(id=contato_id)
+#         return render(request, 'contatos/ver_contato.html', {
+#             'contato': contato
+#         })
+#     except Contato.DoesNotExist as e:
+#         raise Http404
