7a529915dd818b9cdd9edb80d85b3d570034dc49
Italo SF <italosilvafernandesprog@gmail.com>
2022-07-08 06:17:09 -0300

    Acabei de estudar a Seção 10.

--- projetoagenda/agenda/settings.py
+++ projetoagenda/agenda/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
+STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
